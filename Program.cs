c8a1987158b3b5b8ba31fb143b8a1149a5201218
Dominik Pflegerl <github.me@neus.xyz>
2020-01-24 22:41:00 +0100

    - cleand up code - changed Thread.Sleep - added refresh after button click

--- Program.cs
+++ Program.cs
@@ -9,29 +9,27 @@ namespace UberAcceptApp {
 
     internal class Program {
 
-        private static void Main(string[] args) {
+        private static void Main() {
             Console.Title = "Uber";
             string path = @"C:\inetpub\www\uber\rides.txt";
 
             ChromeOptions options = new ChromeOptions();
-
             // profile
             options.AddArgument(@"user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data\\");
-            options.AddArgument("--profile-directory=Profile 1");
-
+            options.AddArgument("--profile-directory=Default");
             // options
             //options.AddArgument("headless");
             options.AddArgument("log-level=3");
-
             IWebDriver driver = new ChromeDriver(options);
-            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+
+
             driver.Navigate().GoToUrl("https://vsdispatch.uber.com");
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
 
             while (true) {
                 //IWebElement button = driver.FindElement(By.CssSelector("#wrapper > div:nth-child(3) > div.jss22.jss26.jss23.jss16 > div.jss53.jss55.jss54 > div > button"));
                 IWebElement button = driver.FindElement(By.XPath("/html/body/div/div/div[3]/div[1]/div[1]/div/button"));
                 bool status = button.Enabled;
-
                 if (status) {
                     button.Click();
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -41,9 +39,8 @@ namespace UberAcceptApp {
                     }
                     Console.ResetColor();
                     Thread.Sleep(5000);
+                    driver.Navigate().Refresh();
                 }
-                //driver.Navigate().Refresh();
-                Thread.Sleep(500);
             }
         }
 
