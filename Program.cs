a2b3e95399b5b0096c9ad648da390cc943f4f423
Dominik Pflegerl <github.me@neus.xyz>
2020-01-27 15:02:20 +0100

    - improved code

--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ using System.Threading;
 namespace UberAcceptApp {
 
     internal class Program {
+        public string path = @"C:\inetpub\www\uber\rides.txt";
 
         private static void Main() {
             Console.Title = "Uber";
-            string path = @"C:\inetpub\www\uber\rides.txt";
+            
 
             ChromeOptions options = new ChromeOptions();
             // profile
             options.AddArgument(@"user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data\\");
-            options.AddArgument("--profile-directory=Default");
+            options.AddArgument("--profile-directory=Profile 1");
             // options
             //options.AddArgument("headless");
             options.AddArgument("log-level=3");
@@ -26,22 +27,36 @@ namespace UberAcceptApp {
             driver.Navigate().GoToUrl("https://vsdispatch.uber.com");
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
 
+            ConsoleStartTime();
+
             while (true) {
                 //IWebElement button = driver.FindElement(By.CssSelector("#wrapper > div:nth-child(3) > div.jss22.jss26.jss23.jss16 > div.jss53.jss55.jss54 > div > button"));
                 IWebElement button = driver.FindElement(By.XPath("/html/body/div/div/div[3]/div[1]/div[1]/div/button"));
                 bool status = button.Enabled;
                 if (status) {
                     button.Click();
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    using (StreamWriter w = File.AppendText(path)) {
-                        Log(w);
-                        Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm") + " | Ride accepted");
-                    }
-                    Console.ResetColor();
-                    Thread.Sleep(5000);
+                    ConsoleWriteAccept();
+
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                     driver.Navigate().Refresh();
-                }
+                    driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+                } 
             }
+        }
+
+        private static void ConsoleWriteAccept() {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            using (StreamWriter w = File.AppendText(@"C:\inetpub\www\uber\rides.txt")) {
+                Log(w);
+                Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm") + " | Ride accepted");
+            }
+            Console.ResetColor();
+        }
+
+        private static void ConsoleStartTime() {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Start Time: " + DateTime.Now.ToString("dd.MM.yyyy HH:mm"));
+            Console.ResetColor();
         }
 
         private static void Log(TextWriter w) {
